b23904a0356ba83dcdad36cc1c955f7612bd5467
NitramX7 <nitramxmb@gmail.com>
2025-11-04 17:34:59 +0100

    cambios en el manifest para que funcione

--- __manifest__.py
+++ __manifest__.py
@@ -22,7 +22,7 @@ Long description of module's purpose
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
     ],
@@ -31,4 +31,3 @@ Long description of module's purpose
         'demo/demo.xml',
     ],
 }
-
