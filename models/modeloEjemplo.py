20858144730746041bd1b3af8b588e0392132ddb
NitramX7 <nitramxmb@gmail.com>
2025-11-05 17:05:51 +0100

    subir cambio de modelos

--- models/modeloEjemplo.py
+++ models/modeloEjemplo.py
@@ -1,8 +1,12 @@
-from odoo import fields, models
+from odoo import models, fields
 
 
 class Ejemplo(models.Model):
     _name = "modelo.de.ejemplo"
-    _descriptio = "Modelo para hacer una prueba"
+    _description = "Modelo para hacer una prueba"
 
+    # Campos que sí usas en las vistas XML
     name = fields.Char(string="Nombre")
+    ex_field = fields.Char(string="Example")
+    ex_field_2 = fields.Char(string="Otro campo")
+    ex_field_3 = fields.Char(string="Oculto")
