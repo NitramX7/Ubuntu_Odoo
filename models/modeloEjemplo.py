22dfd01a4ca22044824a48b2cbb1dbf2d8122dd2
NitramX7 <nitramxmb@gmail.com>
2025-11-06 14:58:24 +0100

    compracion de que funcione

--- models/modeloEjemplo.py
+++ models/modeloEjemplo.py
@@ -1,12 +1,8 @@
-from odoo import models, fields
+from odoo import fields, models
 
 
 class Ejemplo(models.Model):
     _name = "modelo.de.ejemplo"
-    _description = "Modelo para hacer una prueba"
+    _descriptio = "Modelo para hacer una prueba"
 
-    # Campos que sí usas en las vistas XML
     name = fields.Char(string="Nombre")
-    ex_field = fields.Char(string="Example")
-    ex_field_2 = fields.Char(string="Otro campo")
-    ex_field_3 = fields.Char(string="Oculto")
