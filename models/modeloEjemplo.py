635030ece2cd33e5fdffec356ab39fa3c427a127
NitramX <nitramxmb@gmail.com>
2025-11-07 09:08:55 +0100

    errores en el modelo

--- models/modeloEjemplo.py
+++ models/modeloEjemplo.py
@@ -3,6 +3,6 @@ from odoo import fields, models
 
 class Ejemplo(models.Model):
     _name = "modelo.de.ejemplo"
-    _descriptio = "Modelo para hacer una prueba"
+    _description = "Modelo para hacer una prueba"
 
     name = fields.Char(string="Nombre")
